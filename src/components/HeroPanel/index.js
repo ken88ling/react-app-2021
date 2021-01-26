b90e5b2e745187409c852027ba3b7cd03c27b9c6
lingyiaoyang <76722905+lingyiaoyang@users.noreply.github.com>
2021-01-26 23:31:46 +0800

    [hero][nav]

--- src/components/HeroPanel/index.js
+++ src/components/HeroPanel/index.js
@@ -1,6 +1,17 @@
 import React from 'react';
+import { Jumbotron, Button } from 'reactstrap';
 import './hero.css';
 
 export default function HeroPanel() {
-  return <div className='hero-container'></div>;
+  return  <div>
+  <Jumbotron>
+    <h1 className="display-3">Hello, world!</h1>
+    <p className="lead">This is a simple hero unit, a simple Jumbotron-style component for calling extra attention to featured content or information.</p>
+    <hr className="my-2" />
+    <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
+    <p className="lead">
+      <Button color="primary">Learn More</Button>
+    </p>
+  </Jumbotron>
+</div>
 }
--- src/components/Pack.js
+++ src/components/Pack.js
@@ -1,5 +1,105 @@
-import React from 'react';
+import React from "react";
+import {
+  Card,
+  CardImg,
+  CardText,
+  CardBody,
+  CardTitle,
+  CardSubtitle,
+  Button,
+} from "reactstrap";
 
-export default function Pack() {
-  return <div>Pack</div>;
-}
+const Pack = (props) => {
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="col-lg-3 col-md-6 col-sm-12">
+          <Card>
+            <CardImg
+              top
+              width="100%"
+              src="/assets/318x180.svg"
+              alt="Card image cap"
+            />
+            <CardBody>
+              <CardTitle tag="h5">Card title</CardTitle>
+              <CardSubtitle tag="h6" className="mb-2 text-muted">
+                Card subtitle
+              </CardSubtitle>
+              <CardText>
+                Some quick example text to build on the card title and make up
+                the bulk of the card's content.
+              </CardText>
+              <Button>Button</Button>
+            </CardBody>
+          </Card>
+        </div>
+        <div className="col-lg-3 col-md-6 col-sm-12">
+          <Card>
+            <CardImg
+              top
+              width="100%"
+              src="/assets/318x180.svg"
+              alt="Card image cap"
+            />
+            <CardBody>
+              <CardTitle tag="h5">Card title</CardTitle>
+              <CardSubtitle tag="h6" className="mb-2 text-muted">
+                Card subtitle
+              </CardSubtitle>
+              <CardText>
+                Some quick example text to build on the card title and make up
+                the bulk of the card's content.
+              </CardText>
+              <Button>Button</Button>
+            </CardBody>
+          </Card>
+        </div>
+        <div className="col-lg-3 col-md-6 col-sm-12">
+          <Card>
+            <CardImg
+              top
+              width="100%"
+              src="/assets/318x180.svg"
+              alt="Card image cap"
+            />
+            <CardBody>
+              <CardTitle tag="h5">Card title</CardTitle>
+              <CardSubtitle tag="h6" className="mb-2 text-muted">
+                Card subtitle
+              </CardSubtitle>
+              <CardText>
+                Some quick example text to build on the card title and make up
+                the bulk of the card's content.
+              </CardText>
+              <Button>Button</Button>
+            </CardBody>
+          </Card>
+        </div>
+        <div className="col-lg-3 col-md-6 col-sm-12">
+          <Card>
+            <CardImg
+              top
+              width="100%"
+              src="/assets/318x180.svg"
+              alt="Card image cap"
+            />
+            <CardBody>
+              <CardTitle tag="h5">Card title</CardTitle>
+              <CardSubtitle tag="h6" className="mb-2 text-muted">
+                Card subtitle
+              </CardSubtitle>
+              <CardText>
+                Some quick example text to build on the card title and make up
+                the bulk of the card's content.
+              </CardText>
+              <Button>Button</Button>
+            </CardBody>
+          </Card>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Pack;
